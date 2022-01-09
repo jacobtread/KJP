810b52dc046e756240a194bddda9b971dc0f374d
Jacobtread <jacobtread@gmail.com>
2022-01-10 06:01:27 +1300

    Added code documentation

--- app.go
+++ app.go
@@ -7,7 +7,6 @@ import (
 	"github.com/aerogo/aero"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -24,9 +23,8 @@ const UserAgent = "KAMAR/ CFNetwork/ Darwin/"
 const DefaultKey = "vtku"
 
 func main() {
-
+	// Create a new webserver
 	app := aero.New()
-
 	// Maps the pass-through routes and params to KAMAR
 
 	// Middleware for adding CORS Headers
@@ -41,150 +39,164 @@ func main() {
 			return err
 		}
 	})
+	// Bind all methods to HandleRequest
 	app.Any("/api/:command", HandleRequest)
+	// Start the webserver
 	app.Run()
 }
 
+// HandleRequest handles incoming requests and makes KAMAR requests from them
 func HandleRequest(context aero.Context) error {
+	// Get the incoming request
 	request := context.Request()
-
+	// Search for a mapping for the requested command
 	mapping, exists := Mappings[context.Get("command")]
-	if !exists {
-		return context.JSON(ErrorResponse{Error: "Command mapping unknown?!", Code: 0})
+	if !exists { // If the command doesn't exist reply with 404 Not Found
+		context.SetStatus(404)
+		return context.JSON(ErrorResponse{Error: "No mapping found", Code: 0})
 	}
-
 	method := request.Method()
-	if mapping.Method != method {
-		context.SetStatus(405)
+	if mapping.Method != method { // If the request method doesn't match the mapping method
+		context.SetStatus(405) // Set the status to 405 Method Not Allowed
+		// Let them know the method they should be using instead
 		return context.JSON(ErrorResponse{Error: fmt.Sprintf("Unsupported method expected '%s'", mapping.Method)})
 	}
-
+	// Get the Authorization header (used for KAMAR keys)
 	key := request.Header("Authorization")
-	if len(key) < 1 {
-		if mapping.RequireKey == true {
-			context.SetStatus(401)
+	if len(key) < 1 { // If nothing was provided
+		if mapping.RequireKey == true { // If a key is required for this mapping
+			context.SetStatus(403) // Set the status to 403 Forbidden
+			// Let them know the header is required
 			return context.JSON(ErrorResponse{Error: "You must provided a Authorization header to access this route"})
 		}
+		// If it's not required just use the default key "vtku"
 		key = DefaultKey
 	}
-
+	// Get the portal domain from the X-Portal header
 	portalUrl := request.Header("X-Portal")
-	if len(portalUrl) < 1 {
-		context.SetStatus(400)
+	if len(portalUrl) < 1 { // If the header is missing
+		context.SetStatus(400) // Set the status to 400 Bad Request
+		// Let them know that the X-Portal header is required
 		return context.JSON(ErrorResponse{Error: "Missing X-Portal header. Don't know where to send the request"})
 	}
-
+	// Prepend the protocol and append the api route
 	portalUrl = fmt.Sprintf("https://%s/api/api.php", portalUrl)
-
-	params := map[string]interface{}{}
-	if method == "POST" {
+	// The request values to be provided to the KAMAR request
+	values := url.Values{}
+	// Add the Key to the values
+	values.Set("Key", key)
+	// Add the Command to the values
+	values.Set("Command", mapping.Command)
+	if method == "POST" { // If the method is POST we will have post body
+		// This map is used to store the unprocessed JSON
 		rawData := map[string]interface{}{}
-		err := parsePostData(&rawData, context)
-		if err != nil {
-			return err
+		// Get the bytes of the request body
+		bytes, err := context.Request().Body().Bytes()
+		if err != nil { // If we failed to get the body bytes
+			context.SetStatus(400) // Set the status to 400 Bad Request
+			// Let them know they are missing a body when its required
+			return context.JSON(ErrorResponse{Error: "Post request missing request body.", Code: 0})
 		}
+		// Deserialize the JSON body
+		err = json.Unmarshal(bytes, &rawData)
+		if err != nil { // If we failed to deserialize the body
+			context.SetStatus(400) // Set the status to 400 Bad Request
+			// Let them know the JSON they provided is malformed
+			return context.JSON(ErrorResponse{Error: "Malformed request body. Expected JSON", Code: 0})
+		}
+		// Loop over all the mapping parameters
 		for key, value := range mapping.Parameters {
+			// Check if the raw data contains the parameter key
 			data, exists := rawData[key]
-			if value.Required && !exists {
-				context.SetStatus(422)
+			if value.Required && !exists { // If the value is required but is not present
+				context.SetStatus(422) // Set the status to 422 Unprocessable Entity
+				// Let them know that they didn't provide a required field
 				return context.JSON(ErrorResponse{Error: fmt.Sprintf("Missing required field '%s'", key)})
 			} else {
-				if reflect.ValueOf(data).Kind() == reflect.String {
-					if len(data.(string)) < 1 && value.Required {
-						context.SetStatus(422)
+				if reflect.ValueOf(data).Kind() == reflect.String { // If the type is a string
+					if len(data.(string)) < 1 && value.Required { // Check that its not an empty string and required
+						context.SetStatus(422) // Set the status to 422 Unprocessable Entity
+						// Let them know that they didn't provide a required field
 						return context.JSON(ErrorResponse{Error: fmt.Sprintf("Missing required field '%s'", key)})
 					}
 				}
-				params[value.Name] = data
+				// Set the parameter value with the KAMAR naming
+				values.Add(value.Name, data.(string))
 			}
 		}
 
 	} else if method == "GET" {
+		// Loop over all the mapping parameters
 		for key, value := range mapping.Parameters {
+			// Get the value from the query string
 			param := context.Query(key)
-			if value.Required && len(param) < 1 {
-				context.SetStatus(422)
+			if value.Required && len(param) < 1 { // If the value is required but is not present
+				context.SetStatus(422) // Set the status to 422 Unprocessable Entity
+				// Let them know that they didn't provide a required field
 				return context.JSON(ErrorResponse{Error: fmt.Sprintf("Missing required parameter '%s'", key)})
 			}
-			params[value.Name] = param
+			// Set the parameter value with the KAMAR naming
+			values.Add(value.Name, param)
 		}
-	} else {
-		context.SetStatus(405)
-		return context.JSON(ErrorResponse{Error: "Unsupported method only GET and POST are supported."})
 	}
-
-	data := url.Values{}
-	data.Set("Key", key)
-	data.Set("Command", mapping.Command)
-	for key, value := range params {
-		data.Set(key, value.(string))
-	}
-
-	res, err := GetXML(portalUrl, context.IP(), &data)
-	if err != nil {
-		context.SetStatus(500)
+	// Make the request to KAMAR and get back the XML bytes
+	res, err := MakeRequest(portalUrl, context.IP(), &values)
+	if err != nil { // If we got an error while connecting to KAMAR
+		context.SetStatus(500) // Set the status to 500 Server Error
+		// Let them know we failed to connect
 		return context.JSON(ErrorResponse{Error: "Failed to get response from KAMAR", Code: 0})
 	}
-
-	// Debug printy boi for response
-	println(string(res))
-
-	var out = mapping.Response()
+	// Create a new response pointer for the mapping
+	out := mapping.Response()
+	// Deserialize the XML into the mapping response
 	err = xml.Unmarshal(res, &out)
-	if err != nil {
-		fmt.Println(err)
-		context.SetStatus(500)
+	if err != nil { // If we failed to deserialize the XML
+		context.SetStatus(500) // Set the status to 500 Server Error
+		// Let them know we parse the response
 		return context.JSON(ErrorResponse{Error: "Failed to map response from KAMAR", Code: 0})
 	}
-
+	// Return the new JSON mapped response
 	return context.JSON(out)
 }
 
-func parsePostData(rawData *map[string]interface{}, context aero.Context) error {
-	bytes, err := context.Request().Body().Bytes()
-	if err != nil {
-		context.SetStatus(400)
-		return context.JSON(ErrorResponse{Error: "Post request missing request body.", Code: 0})
-	}
-	err = json.Unmarshal(bytes, &rawData)
-	if err != nil {
-		context.SetStatus(400)
-		return context.JSON(ErrorResponse{Error: "Malformed request body. Expected JSON", Code: 0})
-	}
-	return nil
-}
-
-func GetXML(endpoint string, ip string, data *url.Values) ([]byte, error) {
-	if !data.Has("Key") {
-		data.Add("Key", DefaultKey)
-	}
+// MakeRequest Makes an HTTP request to the provided url with the provided data and returns its bytes
+func MakeRequest(endpoint string, ip string, data *url.Values) ([]byte, error) {
+	// Create a new HTTP client
 	client := &http.Client{}
-
+	// Encode the body data
 	encoded := data.Encode()
-
+	// Create a new POST request with the encoded body
 	req, err := http.NewRequest("POST", endpoint, strings.NewReader(encoded))
-	if err != nil {
-		return nil, err
+	if err != nil { // If we encountered an error
+		return nil, err // Return the error
 	}
-	req.Header.Add("User-Agent", "KAMAR/ CFNetwork/ Darwin/")
+	// Set the User-Agent header this is required otherwise
+	// KAMAR will prevent us from making calls to the API
+	req.Header.Add("User-Agent", UserAgent)
+	// Set the Content-Type to application/x-www-form-urlencoded
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	// Set the content length to the body content length
 	req.Header.Add("Content-Length", strconv.Itoa(len(encoded)))
+	// Add a forwarded for header so the server knows we are a proxy for the provided IP
 	req.Header.Add("X-Forwarded-For", ip)
+	// Pretend that the request came from the KAMAR app
 	req.Header.Add("X-Requested-With", "nz.co.KAMAR")
-
+	// Send the HTTP request to KAMAR
 	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
+	if err != nil { // If we encountered an error
+		return nil, err // Return the error
 	}
-	log.Println(res.Status)
+	// Deffer and close the body
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
 		}
 	}(res.Body)
+	// Read all the body data
 	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
+	if err != nil { // If we encountered an error
+		return nil, err // Return the error
 	}
+	// Return the response bytes we got back
 	return body, nil
 }
--- go.mod
+++ go.mod
@@ -15,6 +15,11 @@ require (
 	github.com/akyoto/tty v0.1.4 // indirect
 	github.com/akyoto/uuid v1.1.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/yuin/goldmark v1.4.1 // indirect
 	github.com/zeebo/xxh3 v1.0.1 // indirect
+	golang.org/x/mod v0.5.1 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
+	golang.org/x/tools v0.1.8 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
